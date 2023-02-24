7f6841007400c9ed9630f2da5045cb651ef5eb17
bigth21 <taekhyeon.nam@gmail.com>
2023-02-24 23:16:50 +0900

    Remove public access modifier of bean registration method

--- src/main/java/bigth/myserver/config/encryption/JasyptConfig.java
+++ src/main/java/bigth/myserver/config/encryption/JasyptConfig.java
@@ -6,14 +6,14 @@ import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class JasyptConfig {
 
     private final String ENCRYPTED_KEY = "AQICAHhDd11ecqbBy1k+OZKqV1U3ZMGoRl/WhqdDWXcihgT6hQGTQFDLvZfMMrTkUNlOWtkKAAAAdjB0BgkqhkiG9w0BBwagZzBlAgEAMGAGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMW4AeMqqRsFn82+SRAgEQgDNKlMbpwG3UUopCg87cfYoPqCN61zjMGdOhkowazXEDxpc931JDSRODFlKdG6BTmu6nusE=";
     private final String ALGORITHM = "PBEWithMD5AndDES";
 
     @Bean("jasyptStringEncryptor")
-    public StringEncryptor stringEncryptor() {
+    StringEncryptor stringEncryptor() {
         var encryptor = new PooledPBEStringEncryptor();
         var config = new SimpleStringPBEConfig();
         config.setPassword(KMS.decrypt(ENCRYPTED_KEY));
